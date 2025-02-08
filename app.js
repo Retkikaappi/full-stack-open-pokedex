71056e72db60e2a424a8fc84a511253928d3f41f
Joonas Nuolikoski <joonas.nuolikoski@gmail.com>
2025-02-08 08:42:37 +0200

    add deploy strategy canary

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
 app.get('/version', (req, resp) => {
-  resp.send('1')
+  resp.send('3')
 })
 
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...  '
   res.send('ok')
 })
+
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
