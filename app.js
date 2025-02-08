30e140090a80060175b1c96db652fe3f8fbd3950
Joonas Nuolikoski <joonas.nuolikoski@gmail.com>
2025-02-08 08:04:19 +0200

    test deployment

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+app.get('/version', (req, resp) => {
+  resp.send('1')
+})
+
+// app.get('/health', (req, res) => {
+//   // eslint-disable-next-line no-constant-condition
+//   if (true) throw 'error...  '
+//   res.send('ok')
+// })
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
--- playwright.config.js
+++ playwright.config.js
@@ -76,3 +76,4 @@ export default defineConfig({
     command: 'npm run start-prod',
   },
 })
+
